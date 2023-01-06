7b8d145189b0ea6edabc2e21111b31db10e89012
Angel Sainero Villar <angelsainero@gmail.com>
2023-01-06 18:05:49 +0100

    feat: se crea funcion para insertar barcos

--- index.js
+++ index.js
@@ -21,12 +21,48 @@ function printBoard(board) {
     console.log("└─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴")  
     }
     
-    // Añade algunos barcos al tablero
-    board[2][4] = "X";
-    board[3][4] = "X";
-    board[4][4] = "X";
-    board[5][4] = "X";
+       
+
+    //crear naves
+    // 1 Portaaviones (5 casillas)
+    // 1 Buque (4 casillas)
+    // 2 Submarinos (3 casillas)
+    // 3 Cruceros (2 casillas)
+    // 3 lanchas (1 casilla)
+
+   var aircraftCarrier = {
+    number: 1,
+    size: 5
+   }
+   var vessel = {
+    number: 1,
+    size: 4
+   }
+   var submarine = {
+    number: 2,
+    size: 3
+   }
+   var cruise = {
+    number: 3,
+    size: 2
+   }
+   var boat = {
+    number: 3,
+    size: 1
+   }
+
+    //funcion para insertar barcos  en el board
+   function insertShips (board, ship, row, col) {
     
-    // Muestra el tablero en la consola
-    printBoard(board);
-    
+    for (let i = col; i < col + ship.size; i++){
+      //comprueba que no se salga del tablero
+      if(col + ship.size < board.length) {
+      board[row][i]="X";
+      }else{
+          return false
+          }
+        }
+  }
+  
+  console.log(insertShips(board, aircraftCarrier, 2, 3));
+  printBoard(board);
